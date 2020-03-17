b61f103872628c0cbdb0965ad706178545ed5d63
리나 <sjr0930@gmail.com>
2020-03-17 17:11:34 +0900

    host 변경

--- scrapetest.py
+++ scrapetest.py
@@ -55,7 +55,7 @@ def home():
     return resJSON
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0',debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
+    #app.run(debug=True)
 
 
