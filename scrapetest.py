c9239acb3a95f6b44f9e642943046a3d682ba55d
리나 <sjr0930@gmail.com>
2020-03-17 17:10:36 +0900

    코로나 바이러스 환자현황 스크롤러 하드코딩 버전

--- scrapetest.py
+++ scrapetest.py
@@ -22,21 +22,40 @@ def getTitle(url):
 
 @app.route('/')
 def home():
+    '''
+    api 서버에서 데이터 가져오기
     URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/stores/json?page=1"
     request = urllib.request.Request(URL)
     response = urllib.request.urlopen(request)
     result = response.read().decode('utf-8')
     jsonResult = json.loads(result)
     print(jsonResult)
-    print('test')
-    title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+    '''
+
+    '''
+    title 가져오기
+    예외처리 포함한 예제
+    title = getTitle('http://ncov.mohw.go.kr/')
     if title == None:
         print('TItle could not be found')
     else:
-        print(title)
-    return jsonResult
+        print(type(title))
+    test = {"title" : title.string}
+    '''
+    html = urlopen('http://ncov.mohw.go.kr/')
+    bs = BeautifulSoup(html,'html.parser')
+    numList = bs.findAll('span',{'class':'num'})
+    resultList = []
+    for n in numList[:4]:
+        resultList.append(n.get_text())
+    resultList[0] = resultList[0][4:]
+
+    resJSON = {"accumulate" : resultList[0], "Cure": resultList[1], "underTreatment":resultList[2], "Dead":resultList[3]}
+
+    return resJSON
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    #app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
 
 
